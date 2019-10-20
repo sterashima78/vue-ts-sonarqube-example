265931ecb18ec2e5ff60178f4dfece290f77c8dc
Terashima Shin <sterashima78@gmail.com>
2019-10-20 18:15:24 +0900

    init

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel"
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  testResultsProcessor: "jest-sonar-reporter",
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx,vue}",
+    "!<rootDir>/node_modules/"
+  ]
 };
